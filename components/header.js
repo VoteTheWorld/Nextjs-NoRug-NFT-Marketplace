2c8db44ab1daaa4fc1e8b7596ee36a9832719f3e
xudong wang <2764413w@student.gla.ac.uk>
2023-02-08 23:37:30 +0000

    inital commit

--- components/header.js
+++ components/header.js
@@ -3,14 +3,17 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav>
-            <Link href="/">
-                <a> NoRug NFT Marketplace</a>
-            </Link>
-            <Link href="/sell-page">
-                <a> Sell NFT</a>
-            </Link>
-            <ConnectButton />
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-4xl">NFT Marketplace</h1>
+            <div className="flex flex-row items-center">
+                <Link href="/">
+                    <a className="mr-4 p-6 text-2xl">Home</a>
+                </Link>
+                <Link href="/sell-page">
+                    <a className="mr-4 p-6 text-2xl">Sell NFT</a>
+                </Link>
+                <ConnectButton moralisAuth={false} />
+            </div>
         </nav>
     )
 }
